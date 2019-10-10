9a5ad6656879c2a6a84c12e9523ada6038eb75c6
FAUSTO <FAUSTO@FAUSTO-PC>
2019-10-10 19:36:17 +0200

    Prepared UI

--- PosteItaliane/src/application/Main.java
+++ PosteItaliane/src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,14 +13,30 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("TotemLayout.fxml"));
-			Scene scene = new Scene(root,400,400);
+			HBox root = (HBox)FXMLLoader.load(getClass().getResource("TotemLayout.fxml"));
+			primaryStage.setMinHeight(700);
+			primaryStage.setMinWidth(700);
+			primaryStage.setFullScreen(true);
+			Scene scene = new Scene(root,700,750);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			prepareUI(scene);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void prepareUI(Scene scene) {
+		scene.heightProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 
 	public static void main(String[] args) {
