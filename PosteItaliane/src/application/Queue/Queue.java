89f68452ca19f9b773aa47a485c9897688a30b6b
Utente <Utente@MULTI02>
2019-10-22 12:17:33 +0200

    Finished Queue

--- PosteItaliane/src/application/Queue/Queue.java
+++ PosteItaliane/src/application/Queue/Queue.java
@@ -1,5 +1,33 @@
 package application.Queue;
 
-public class Queue {
+public class Queue<Type> {
+
+	private Node<Type> head = null;
+	private Node<Type> end = null;
+
+	public void push(Type newObject){
+		Node<Type> newEnd = new Node<Type>(newObject, null);
+		if(end == null){
+			end = newEnd;
+		}
+		else{
+			end.setNext(newEnd);
+			end = end.getNext();
+		}
+		if(head == null){
+			head = end;
+		}
+	}
+
+	public Type pop(){
+		if(head != null){
+			Type object = head.getData();
+			head = head.getNext();
+			if(head == null)
+				end = null;
+			return object;
+		}
+		return null;
+	}
 
 }
