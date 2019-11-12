fdf9abd4c7796a5b87a440f2f991bc8af93fce88
FAUSTO <FAUSTO@FAUSTO-PC>
2019-11-12 21:13:53 +0100

    Removed commented methods

--- PosteItaliane/src/application/TotemController.java
+++ PosteItaliane/src/application/TotemController.java
@@ -10,58 +10,9 @@ public class TotemController {
 	private long deliveryCount = 0;
 	private long singleOperationCount = 0;
 	
-	/*
-	private Queue<Request> waitingCreditRequests = new Queue<Request>();
-	private Queue<Request> waitingPaymentRequests = new Queue<Request>();
-	private Queue<Request> waitingDeliveryRequests = new Queue<Request>();
-	private Queue<Request> waitingPriorityRequests = new Queue<Request>();
-
-	boolean[] hasTimePassedFlags = {true,true,true,true};
-	*/
-	
 	public void bootDispenser() {
 		dispenserTR.start();
 	}
-	/*
-	public void runTotem(){
-		Runnable test = new Runnable() {
-
-			@Override
-			public void run() {
-				
-				for(;;) {
-					System.out.print("test");
-					try {
-						Thread.sleep(160);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					if(hasTimePassedFlags[0]) {
-						hasTimePassedFlags[0] = false;
-						String text="e";
-						for(long i = 0;i<waitingCreditRequests.lenght();i++)
-							text+=waitingCreditRequests.get().getName()+"\n";
-						creditCardList.setText(text);
-						if(waitingCreditRequests.get() != null)
-							new Timer().schedule(new TimerTask() {
-								@Override
-								public void run() {
-									hasTimePassedFlags[0] = true;
-									waitingCreditRequests.pop();
-								}
-							}, waitingCreditRequests.get().getTime());
-					}
-				}
-				
-			}
-			
-		};
-		Thread test2 = new Thread(test);
-		test2.start();
-		
-	}
-	*/
 	
 	synchronized private void sendRequest(Request request) {
 		dispenser.pushToQueue(request);
@@ -78,25 +29,21 @@ public class TotemController {
 		String request = "CP"+ ++creditCount;
 		Request newObject = new Request(request,randomTimer());
 		sendRequest(newObject);
-		//waitingCreditRequests.push(newObject);
 	}
 	public void paymentRequested() {
 		String request = "P"+ ++paymentCount;
 		Request newObject = new Request(request,randomTimer());
 		sendRequest(newObject);
-		//waitingPaymentRequests.push(newObject);
 	}
 	public void deliveryRequested() {
 		String request = "SR"+ ++deliveryCount;
 		Request newObject = new Request(request,randomTimer());
 		sendRequest(newObject);
-		//waitingDeliveryRequests.push(newObject);
 	}
 	public void singleOperationRequested() {
 		String request = "U"+ ++singleOperationCount;
 		Request newObject = new Request(request,randomTimer());
 		sendRequest(newObject);
-		//waitingPriorityRequests.push(newObject);
 	}
 
 	
